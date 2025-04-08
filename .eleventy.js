e3f62b4af1fbd9f5d1cfdb7155595e09d10646f7
Zach Stucky <zkstucky@gmail.com>
2025-04-08 10:09:00 -0500

    Adds event and contact pages and nav bar

--- .eleventy.js
+++ .eleventy.js
@@ -2,8 +2,11 @@ module.exports = function(eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy("styles.css");
     eleventyConfig.addPassthroughCopy("script.js");
+    eleventyConfig.addPassthroughCopy("contact.html");
+    eleventyConfig.addPassthroughCopy("events.html");
   
     return {
+      templateFormats: ["html", "njk", "md"],
       dir: {
         input: ".",
         output: "_site"
